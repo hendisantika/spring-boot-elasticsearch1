d40fdf3302a3389d1fe459784272f3a4693cd690
hendisantika <hendisantika@yahoo.co.id>
2024-12-15 10:22:31 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/elasticsearch/SpringBootElasticsearch1Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticsearch1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootElasticsearch1Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/elasticsearch/SpringBootElasticsearch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticsearch1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
