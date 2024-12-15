cc940a81153c3d6fc9c39d72385b4a5590676392
hendisantika <hendisantika@yahoo.co.id>
2024-12-15 10:25:53 +0700

    Add Product🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/elasticsearch/Product.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.elasticsearch;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/12/24
+ * Time: 10.24
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Document(indexName = "products")
+public class Product {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text, name = "name")
+    private String name;
+
+    @Field(type = FieldType.Text, name = "description")
+    private String description;
+
+    @Field(type = FieldType.Double, name = "price")
+    private double price;
+
+}
