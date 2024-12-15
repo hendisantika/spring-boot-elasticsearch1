657e1c67c5bbd2f672fa5146a60bb30ee8a8aef1
hendisantika <hendisantika@yahoo.co.id>
2024-12-15 10:27:03 +0700

    Add createOrUpdateDocument in ElasticSearchQuery🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/elasticsearch/ElasticSearchQuery.java
+++ src/main/java/id/my/hendisantika/elasticsearch/ElasticSearchQuery.java
@@ -1,8 +1,11 @@
 package id.my.hendisantika.elasticsearch;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +24,18 @@ public class ElasticSearchQuery {
     private final ElasticsearchClient elasticsearchClient;
 
     private final String indexName = "products";
+
+    public String createOrUpdateDocument(Product product) throws IOException {
+        IndexResponse response = elasticsearchClient.index(i -> i
+                .index(indexName)
+                .id(product.getId())
+                .document(product)
+        );
+        if (response.result().name().equals("Created")) {
+            return "Document has been successfully created.";
+        } else if (response.result().name().equals("Updated")) {
+            return "Document has been successfully updated.";
+        }
+        return "Error while performing the operation.";
+    }
 }
