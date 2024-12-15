4c28968776cd3ff1e1e9481263fdd3271dc21de8
hendisantika <hendisantika@yahoo.co.id>
2024-12-15 10:24:28 +0700

    Add ElasticSearchConfiguration🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/elasticsearch/ElasticSearchConfiguration.java
@@ -0,0 +1,43 @@
+package id.my.hendisantika.elasticsearch;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/12/24
+ * Time: 10.24
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class ElasticSearchConfiguration {
+    @Bean
+    public RestClient getRestClient() {
+        RestClient restClient = RestClient.builder(
+                new HttpHost("localhost", 9200)).build();
+        return restClient;
+    }
+
+    @Bean
+    public ElasticsearchTransport getElasticsearchTransport() {
+        return new RestClientTransport(
+                getRestClient(), new JacksonJsonpMapper());
+    }
+
+
+    @Bean
+    public ElasticsearchClient getElasticsearchClient() {
+        ElasticsearchClient client = new ElasticsearchClient(getElasticsearchTransport());
+        return client;
+    }
+}
